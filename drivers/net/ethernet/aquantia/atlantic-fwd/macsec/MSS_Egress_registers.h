001da1a297709aec3ad1277a0a100a0ebba01de1
Mark Starovoytov <mstarovoitov@marvell.com>
2019-12-19 09:52:47 +0300

    ATLDRV-1250 Remove excessive comments in MACSec code

--- drivers/net/ethernet/aquantia/atlantic-fwd/macsec/MSS_Egress_registers.h
+++ drivers/net/ethernet/aquantia/atlantic-fwd/macsec/MSS_Egress_registers.h
@@ -1,7 +1,6 @@
 #ifndef MSS_EGRESS_REGS_HEADER
 #define MSS_EGRESS_REGS_HEADER
 
-
 #define mssEgressControlRegister_ADDR 0x00005002
 #define mssEgressSaExpiredStatusRegister_ADDR 0x00005060
 #define mssEgressSaThresholdExpiredStatusRegister_ADDR 0x00005062
@@ -9,227 +8,61 @@
 #define mssEgressLutControlRegister_ADDR 0x00005081
 #define mssEgressLutDataControlRegister_ADDR 0x000050A0
 
-//---------------------------------------------------------------------------------
-//                  MSS Egress Control Register: 1E.5002 
-//---------------------------------------------------------------------------------
-struct mssEgressControlRegister_t
-{
-  union
-  {
-    struct
-    {
-                    /*! \brief 1E.5002.0 R/W MSS Egress Soft Reset
-                        mssEgressControlRegister_t.bits_0.mssEgressSoftReset
-                        Default = 0x0
-                        1 = Soft reset
-                 <B>Notes:</B>
-                        S/W reset  */
-      unsigned int   mssEgressSoftReset : 1;    // 1E.5002.0  R/W      Default = 0x0 
-                     /* 1 = Soft reset
-                          */
-                    /*! \brief 1E.5002.1 R/W MSS Egress Drop KAY Packet
-                        mssEgressControlRegister_t.bits_0.mssEgressDropKayPacket
-                        Default = 0x0
-                        1 = Drop KAY packet
-                 <B>Notes:</B>
-                        Decides whether KAY packets have to be dropped  */
-      unsigned int   mssEgressDropKayPacket : 1;    // 1E.5002.1  R/W      Default = 0x0 
-                     /* 1 = Drop KAY packet
-                          */
-                    /*! \brief 1E.5002.2 R/W MSS Egress Drop EGPRC LUT Miss
-                        mssEgressControlRegister_t.bits_0.mssEgressDropEgprcLutMiss
-                        Default = 0x0
-                        1 = Drop Egress Classification LUT miss packets
-                 <B>Notes:</B>
-                        Decides whether Egress Pre-Security Classification (EGPRC) LUT miss packets are to be dropped  */
-      unsigned int   mssEgressDropEgprcLutMiss : 1;    // 1E.5002.2  R/W      Default = 0x0 
-                     /* 1 = Drop Egress Classification LUT miss packets */
-                    /*! \brief 1E.5002.3 R/W MSS Egress GCM Start
-                        mssEgressControlRegister_t.bits_0.mssEgressGcmStart
-                        Default = 0x0
-                        1 = Start GCM
-                 <B>Notes:</B>
-                        Indicates GCM to start  */
-      unsigned int   mssEgressGcmStart : 1;    // 1E.5002.3  R/W      Default = 0x0 
-                     /* 1 = Start GCM */
-                    /*! \brief 1E.5002.4 R/W MSS Egresss GCM Test Mode
-                        mssEgressControlRegister_t.bits_0.mssEgresssGcmTestMode
-                        Default = 0x0
-                        1 = Enable GCM test mode
-                 <B>Notes:</B>
-                        Enables GCM test mode  */
-      unsigned int   mssEgresssGcmTestMode : 1;    // 1E.5002.4  R/W      Default = 0x0 
-                     /* 1 = Enable GCM test mode */
-                    /*! \brief 1E.5002.5 R/W MSS Egress Unmatched Use SC 0
-                        mssEgressControlRegister_t.bits_0.mssEgressUnmatchedUseSc_0
-                        Default = 0x0
-                        1 = Use SC 0 for unmatched packets
-                        0 = Unmatched packets are uncontrolled packets
-                 <B>Notes:</B>
-                        Use SC-Index 0 as default SC for unmatched packets. Otherwise the packets are treated as uncontrolled packets.  */
-      unsigned int   mssEgressUnmatchedUseSc_0 : 1;    // 1E.5002.5  R/W      Default = 0x0 
-                     /* 1 = Use SC 0 for unmatched packets
-                        0 = Unmatched packets are uncontrolled packets */
-                    /*! \brief 1E.5002.6 R/W MSS Egress Drop Invalid SA/SC Packets
-                        mssEgressControlRegister_t.bits_0.mssEgressDropInvalidSa_scPackets
-                        Default = 0x0
-                        1 = Drop invalid SA/SC packets
-                 <B>Notes:</B>
-                        Enables dropping of invalid SA/SC packets.  */
-      unsigned int   mssEgressDropInvalidSa_scPackets : 1;    // 1E.5002.6  R/W      Default = 0x0 
-                     /* 1 = Drop invalid SA/SC packets */
-                    /*! \brief 1E.5002.7 R/W MSS Egress Explicit SECTag Report Short Length
-                        mssEgressControlRegister_t.bits_0.mssEgressExplicitSectagReportShortLength
-                        Default = 0x0
-                        Reserved
-                 <B>Notes:</B>
-                        Unused.  */
-      unsigned int   mssEgressExplicitSectagReportShortLength : 1;    // 1E.5002.7  R/W      Default = 0x0 
-                     /* Reserved */
-                    /*! \brief 1E.5002.8 R/W MSS Egress External Classification Enable
-                        mssEgressControlRegister_t.bits_0.mssEgressExternalClassificationEnable
-                        Default = 0x0
-                        1 = Drop EGPRC miss packets
-                 <B>Notes:</B>
-                        If set, internal classification is bypassed. Should always be set to 0.  */
-      unsigned int   mssEgressExternalClassificationEnable : 1;    // 1E.5002.8  R/W      Default = 0x0 
-                     /* 1 = Drop EGPRC miss packets */
-                    /*! \brief 1E.5002.9 R/W MSS Egress ICV LSB 8 Bytes Enable
-                        mssEgressControlRegister_t.bits_0.mssEgressIcvLsb_8BytesEnable
-                        Default = 0x0
-                        1 = Use LSB
-                        0 = Use MSB
-                 <B>Notes:</B>
-                        This bit selects MSB or LSB 8 bytes selection in the case where the ICV is 8 bytes.
-                        0 = MSB is used.  */
-      unsigned int   mssEgressIcvLsb_8BytesEnable : 1;    // 1E.5002.9  R/W      Default = 0x0 
-                     /* 1 = Use LSB
-                        0 = Use MSB  */
-                    /*! \brief 1E.5002.A R/W MSS Egress High Priority
-                        mssEgressControlRegister_t.bits_0.mssEgressHighPriority
-                        Default = 0x0
-                        1 = MIB counter clear on read enable
-                 <B>Notes:</B>
-                        If this bit is set to 1, read is given high priority and the MIB count value becomes 0 after read.  */
-      unsigned int   mssEgressHighPriority : 1;    // 1E.5002.A  R/W      Default = 0x0 
-                     /* 1 = MIB counter clear on read enable */
-                    /*! \brief 1E.5002.B R/W MSS Egress Clear Counter
-                        mssEgressControlRegister_t.bits_0.mssEgressClearCounter
-                        Default = 0x0
-                        1 = Clear all MIB counters
-                 <B>Notes:</B>
-                        If this bit is set to 1, all MIB counters will be cleared.  */
-      unsigned int   mssEgressClearCounter : 1;    // 1E.5002.B  R/W      Default = 0x0 
-                     /* 1 = Clear all MIB counters */
-                    /*! \brief 1E.5002.C R/W MSS Egress Clear Global Time
-                        mssEgressControlRegister_t.bits_0.mssEgressClearGlobalTime
-                        Default = 0x0
-                        1 = Clear global time
-                 <B>Notes:</B>
-                        Clear global time.  */
-      unsigned int   mssEgressClearGlobalTime : 1;    // 1E.5002.C  R/W      Default = 0x0 
-                     /* 1 = Clear global time */
-                    /*! \brief 1E.5002.F:D R/W MSS Egress Ethertype Explicit SECTag LSB [2:0]
-                        mssEgressControlRegister_t.bits_0.mssEgressEthertypeExplicitSectagLsb
-                        Default = 0x0
-                        Ethertype for explicit SECTag bits 2:0.
-                 <B>Notes:</B>
-                        Ethertype for explicity SECTag.  */
-      unsigned int   mssEgressEthertypeExplicitSectagLsb : 3;    // 1E.5002.F:D  R/W      Default = 0x0 
-                     /* Ethertype for explicit SECTag bits 2:0. */
-    } bits_0;
-    unsigned short word_0;
-  };
-  union
-  {
-    struct
-    {
-                    /*! \brief 1E.5003.C:0 R/W MSS Egress Ethertype Explicit SECTag MSB  [F:3]
-                        mssEgressControlRegister_t.bits_1.mssEgressEthertypeExplicitSectagMsb
-                        Default = 0x0000
-                        Ethertype for explicit SECTag bits 15:3.
-                 <B>Notes:</B>
-                        Ethertype for explicity SECTag.  */
-      unsigned int   mssEgressEthertypeExplicitSectagMsb : 13;    // 1E.5003.C:0  R/W      Default = 0x0000 
-                     /* Ethertype for explicit SECTag bits 15:3. */
-      unsigned int   reserved0 : 3;
-    } bits_1;
-    unsigned short word_1;
-  };
+struct mssEgressControlRegister_t {
+	union {
+		struct {
+			unsigned int mssEgressSoftReset : 1;
+			unsigned int mssEgressDropKayPacket : 1;
+			unsigned int mssEgressDropEgprcLutMiss : 1;
+			unsigned int mssEgressGcmStart : 1;
+			unsigned int mssEgresssGcmTestMode : 1;
+			unsigned int mssEgressUnmatchedUseSc_0 : 1;
+			unsigned int mssEgressDropInvalidSa_scPackets : 1;
+			unsigned int reserved0 : 1;
+			/* Should always be set to 0. */
+			unsigned int mssEgressExternalClassificationEnable : 1;
+			unsigned int mssEgressIcvLsb_8BytesEnable : 1;
+			unsigned int mssEgressHighPriority : 1;
+			unsigned int mssEgressClearCounter : 1;
+			unsigned int mssEgressClearGlobalTime : 1;
+			unsigned int mssEgressEthertypeExplicitSectagLsb : 3;
+		} bits_0;
+		unsigned short word_0;
+	};
+	union {
+		struct {
+			unsigned int mssEgressEthertypeExplicitSectagMsb : 13;
+			unsigned int reserved0 : 3;
+		} bits_1;
+		unsigned short word_1;
+	};
 };
 
-//---------------------------------------------------------------------------------
-//! \brief                MSS Egress LUT Address Control Register: 1E.5080 
-//                  MSS Egress LUT Address Control Register: 1E.5080 
-//---------------------------------------------------------------------------------
-struct mssEgressLutAddressControlRegister_t
-{
-  union
-  {
-    struct
-    {
-                    /*! \brief 1E.5080.8:0 R/W MSS Egress LUT Address [8:0]
-                        mssEgressLutAddressControlRegister_t.bits_0.mssEgressLutAddress
-
-                        Default = 0x000
-
-                        LUT address
-                        
-  */
-      unsigned int   mssEgressLutAddress : 9;    // 1E.5080.8:0  R/W      Default = 0x000 
-                     /* LUT address
-                          */
-      unsigned int   reserved0 : 3;
-                    /*! \brief 1E.5080.F:C R/W MSS Egress LUT Select [3:0]
-                        mssEgressLutAddressControlRegister_t.bits_0.mssEgressLutSelect
-
-                        Default = 0x0
-
-                        LUT select
-                        
-
-                 <B>Notes:</B>
-                        0x0 : Egress MAC Control FIlter (CTLF) LUT
-                        0x1 : Egress Classification LUT
-                        0x2 : Egress SC/SA LUT
-                        0x3 : Egress SMIB  */
-      unsigned int   mssEgressLutSelect : 4;    // 1E.5080.F:C  R/W      Default = 0x0 
-                     /* LUT select
-                          */
-    } bits_0;
-    unsigned short word_0;
-  };
+struct mssEgressLutAddressControlRegister_t {
+	union {
+		struct {
+			unsigned int mssEgressLutAddress : 9;
+			unsigned int reserved0 : 3;
+			/* 0x0 : Egress MAC Control FIlter (CTLF) LUT
+			 * 0x1 : Egress Classification LUT
+			 * 0x2 : Egress SC/SA LUT
+			 * 0x3 : Egress SMIB
+			 */
+			unsigned int mssEgressLutSelect : 4;
+		} bits_0;
+		unsigned short word_0;
+	};
 };
 
-//---------------------------------------------------------------------------------
-//                  MSS Egress LUT Control Register: 1E.5081 
-//---------------------------------------------------------------------------------
-struct mssEgressLutControlRegister_t
-{
-  union
-  {
-    struct
-    {
-      unsigned int   reserved0 : 14;
-                    /*! \brief 1E.5081.E R/W MSS Egress LUT Read
-                        mssEgressLutControlRegister_t.bits_0.mssEgressLutRead
-                        Default = 0x0
-                        1 = LUT read
-                 <B>Notes:</B>
-                        Setting this bit to 1, will read the LUT. This bit will automatically clear to 0.  */
-      unsigned int   mssEgressLutRead : 1;    // 1E.5081.E  R/W      Default = 0x0 
-                     /* 1 = LUT read */
-                    /*! \brief 1E.5081.F R/W MSS Egress LUT Write
-                        mssEgressLutControlRegister_t.bits_0.mssEgressLutWrite
-                        Default = 0x0
-                        1 = LUT write
-                 <B>Notes:</B>
-                        Setting this bit to 1, will write the LUT. This bit will automatically clear to 0.  */
-      unsigned int   mssEgressLutWrite : 1;    // 1E.5081.F  R/W      Default = 0x0 
-                     /* 1 = LUT write */
-    } bits_0;
-    unsigned short word_0;
-  };
+struct mssEgressLutControlRegister_t {
+	union {
+		struct {
+			unsigned int reserved0 : 14;
+			unsigned int mssEgressLutRead : 1;
+			unsigned int mssEgressLutWrite : 1;
+		} bits_0;
+		unsigned short word_0;
+	};
 };
 
 #endif /* MSS_EGRESS_REGS_HEADER */
--- drivers/net/ethernet/aquantia/atlantic-fwd/macsec/MSS_Ingress_registers.h
+++ drivers/net/ethernet/aquantia/atlantic-fwd/macsec/MSS_Ingress_registers.h
@@ -6,209 +6,67 @@
 #define mssIngressLutControlRegister_ADDR 0x00008081
 #define mssIngressLutDataControlRegister_ADDR 0x000080A0
 
-//---------------------------------------------------------------------------------
-//                  MSS Ingress Control Register: 1E.800E 
-//---------------------------------------------------------------------------------
-struct mssIngressControlRegister_t
-{
-  union
-  {
-    struct
-    {
-                    /*! \brief 1E.800E.0 R/W MSS Ingress Soft Reset
-                        mssIngressControlRegister_t.bits_0.mssIngressSoftReset
-                        Default = 0x0
-                        1 = Soft reset
-                 <B>Notes:</B>
-                        S/W reset  */
-      unsigned int   mssIngressSoftReset : 1;    // 1E.800E.0  R/W      Default = 0x0 
-                     /* 1 = Soft reset */
-                    /*! \brief 1E.800E.1 R/W MSS Ingress Operation Point To Point
-                        mssIngressControlRegister_t.bits_0.mssIngressOperationPointToPoint
-                        Default = 0x0
-                        1 = Enable the SCI for authorization default
-                 <B>Notes:</B>
-                        The default SCI for authorization is configured in  See MSS Ingress SCI Default [F:0]   See MSS Ingress SCI Default [1F:10] , See MSS Ingress SCI Default [2F:20] , and  See MSS Ingress SCI Default [3F:30] .  */
-      unsigned int   mssIngressOperationPointToPoint : 1;    // 1E.800E.1  R/W      Default = 0x0 
-                     /* 1 = Enable the SCI for authorization default  */
-                    /*! \brief 1E.800E.2 R/W MSS Ingress Create SCI
-                        mssIngressControlRegister_t.bits_0.mssIngressCreateSci
-                        Default = 0x0
-                        0 = SCI from IGPRC LUT
-                 <B>Notes:</B>
-                        If the SCI is not in the packet and this bit is set to 0, the SCI will be taken from the IGPRC LUT.  */
-      unsigned int   mssIngressCreateSci : 1;    // 1E.800E.2  R/W      Default = 0x0 
-                     /* 0 = SCI from IGPRC LUT
-                          */
-                    /*! \brief 1E.800E.3 R/W MSS Ingress Mask Short Length Error
-                        mssIngressControlRegister_t.bits_0.mssIngressMaskShortLengthError
-                        Default = 0x0
-                        Unused
-                 <B>Notes:</B>
-                        Unused  */
-      unsigned int   mssIngressMaskShortLengthError : 1;    // 1E.800E.3  R/W      Default = 0x0 
-                     /* Unused */
-                    /*! \brief 1E.800E.4 R/W MSS Ingress Drop Kay Packet
-                        mssIngressControlRegister_t.bits_0.mssIngressDropKayPacket
-                        Default = 0x0
-                        1 = Drop KaY packets
-                 <B>Notes:</B>
-                        Decides whether KaY packets have to be dropped  */
-      unsigned int   mssIngressDropKayPacket : 1;    // 1E.800E.4  R/W      Default = 0x0 
-                     /* 1 = Drop KaY packets   */
-                    /*! \brief 1E.800E.5 R/W MSS Ingress Drop IGPRC Miss
-                        mssIngressControlRegister_t.bits_0.mssIngressDropIgprcMiss
-                        Default = 0x0
-                        1 = Drop IGPRC miss packets
-                 <B>Notes:</B>
-                        Decides whether Ingress Pre-Security Classification (IGPRC) LUT miss packets are to be dropped  */
-      unsigned int   mssIngressDropIgprcMiss : 1;    // 1E.800E.5  R/W      Default = 0x0 
-                     /* 1 = Drop IGPRC miss packets */
-                    /*! \brief 1E.800E.6 R/W MSS Ingress Check ICV
-                        mssIngressControlRegister_t.bits_0.mssIngressCheckIcv
-                        Default = 0x0
-                        Unused
-                 <B>Notes:</B>
-                        Unused  */
-      unsigned int   mssIngressCheckIcv : 1;    // 1E.800E.6  R/W      Default = 0x0 
-                     /* Unused */
-                    /*! \brief 1E.800E.7 R/W MSS Ingress Clear Global Time
-                        mssIngressControlRegister_t.bits_0.mssIngressClearGlobalTime
-                        Default = 0x0
-                        1 = Clear global time
-                 <B>Notes:</B>
-                        Clear global time  */
-      unsigned int   mssIngressClearGlobalTime : 1;    // 1E.800E.7  R/W      Default = 0x0 
-                     /* 1 = Clear global time */
-                    /*! \brief 1E.800E.8 R/W MSS Ingress Clear Count
-                        mssIngressControlRegister_t.bits_0.mssIngressClearCount
-                        Default = 0x0
-                        1 = Clear all MIB counters
-                 <B>Notes:</B>
-                        If this bit is set to 1, all MIB counters will be cleared.  */
-      unsigned int   mssIngressClearCount : 1;    // 1E.800E.8  R/W      Default = 0x0 
-                     /* 1 = Clear all MIB counters                        */
-                    /*! \brief 1E.800E.9 R/W MSS Ingress High Priority
-                        mssIngressControlRegister_t.bits_0.mssIngressHighPriority
-                        Default = 0x0
-                        1 = MIB counter clear on read enable
-                 <B>Notes:</B>
-                        If this bit is set to 1, read is given high priority and the MIB count value becomes 0 after read.  */
-      unsigned int   mssIngressHighPriority : 1;    // 1E.800E.9  R/W      Default = 0x0 
-                     /* 1 = MIB counter clear on read enable */
-                    /*! \brief 1E.800E.A R/W MSS Ingress Remove SECTag
-                        mssIngressControlRegister_t.bits_0.mssIngressRemoveSectag
-                        Default = 0x0
-                        1 = Enable removal of SECTag
-                 <B>Notes:</B>
-                        If this bit is set and either of the following two conditions occurs, the SECTag will be removed.
-                        Controlled packet and either the SA or SC is invalid.
-                        IGPRC miss.  */
-      unsigned int   mssIngressRemoveSectag : 1;    // 1E.800E.A  R/W      Default = 0x0 
-                     /* 1 = Enable removal of SECTag */
-                    /*! \brief 1E.800E.C:B R/W MSS Ingress Global Validate Frames [1:0]
-                        mssIngressControlRegister_t.bits_0.mssIngressGlobalValidateFrames
-                        Default = 0x0
-                        Default validate frames configuration
-                 <B>Notes:</B>
-                        If the SC is invalid or if an IGPRC miss packet condition occurs, this default will be used for the validate frames configuration instead of the validate frame entry in the Ingress SC Table (IGSCT).  */
-      unsigned int   mssIngressGlobalValidateFrames : 2;    // 1E.800E.C:B  R/W      Default = 0x0 
-                     /* Default validate frames configuration
-                          */
-                    /*! \brief 1E.800E.D R/W MSS Ingress ICV LSB 8 Bytes Enable
-                        mssIngressControlRegister_t.bits_0.mssIngressIcvLsb_8BytesEnable
-                        Default = 0x0
-                        1 = Use LSB
-                        0 = Use MSB
-                 <B>Notes:</B>
-                        This bit selects MSB or LSB 8 bytes selection in the case where the ICV is 8 bytes.
-                        0 = MSB is used.  */
-      unsigned int   mssIngressIcvLsb_8BytesEnable : 1;    // 1E.800E.D  R/W      Default = 0x0 
-                     /* 1 = Use LSB
-                        0 = Use MSB */
-      unsigned int   reserved0 : 2;
-    } bits_0;
-    unsigned short word_0;
-  };
-  union
-  {
-    struct
-    {
-      unsigned int   reserved0 : 16;
-    } bits_1;
-    unsigned short word_1;
-  };
+struct mssIngressControlRegister_t {
+	union {
+		struct {
+			unsigned int mssIngressSoftReset : 1;
+			unsigned int mssIngressOperationPointToPoint : 1;
+			unsigned int mssIngressCreateSci : 1;
+			/* Unused  */
+			unsigned int mssIngressMaskShortLengthError : 1;
+			unsigned int mssIngressDropKayPacket : 1;
+			unsigned int mssIngressDropIgprcMiss : 1;
+			/* Unused  */
+			unsigned int mssIngressCheckIcv : 1;
+			unsigned int mssIngressClearGlobalTime : 1;
+			unsigned int mssIngressClearCount : 1;
+			unsigned int mssIngressHighPriority : 1;
+			unsigned int mssIngressRemoveSectag : 1;
+			unsigned int mssIngressGlobalValidateFrames : 2;
+			unsigned int mssIngressIcvLsb_8BytesEnable : 1;
+			unsigned int reserved0 : 2;
+		} bits_0;
+		unsigned short word_0;
+	};
+	union {
+		struct {
+			unsigned int reserved0 : 16;
+		} bits_1;
+		unsigned short word_1;
+	};
 };
 
-//---------------------------------------------------------------------------------
-//                  MSS Ingress LUT Address Control Register: 1E.8080 
-//---------------------------------------------------------------------------------
-struct mssIngressLutAddressControlRegister_t
-{
-  union
-  {
-    struct
-    {
-                    /*! \brief 1E.8080.8:0 R/W MSS Ingress LUT Address [8:0]
-                        mssIngressLutAddressControlRegister_t.bits_0.mssIngressLutAddress
-                        Default = 0x000
-                        LUT address  */
-      unsigned int   mssIngressLutAddress : 9;    // 1E.8080.8:0  R/W      Default = 0x000 
-                     /* LUT address
-                          */
-      unsigned int   reserved0 : 3;
-                    /*! \brief 1E.8080.F:C R/W MSS Ingress LUT Select [3:0]
-                        mssIngressLutAddressControlRegister_t.bits_0.mssIngressLutSelect
-                        Default = 0x0
-                        LUT select
-                 <B>Notes:</B>
-                        0x0 : Ingress Pre-Security MAC Control FIlter (IGPRCTLF) LUT
-                        0x1 : Ingress Pre-Security Classification LUT (IGPRC)
-                        0x2 : Ingress Packet Format (IGPFMT) SAKey LUT
-                        0x3 : Ingress Packet Format (IGPFMT) SC/SA LUT
-                        0x4 : Ingress Post-Security Classification LUT (IGPOC)
-                        0x5 : Ingress Post-Security MAC Control Filter (IGPOCTLF) LUT
-                        0x6 : Ingress MIB (IGMIB)  */
-      unsigned int   mssIngressLutSelect : 4;    // 1E.8080.F:C  R/W      Default = 0x0 
-                     /* LUT select
-                          */
-    } bits_0;
-    unsigned short word_0;
-  };
+struct mssIngressLutAddressControlRegister_t {
+	union {
+		struct {
+			unsigned int mssIngressLutAddress : 9;
+			unsigned int reserved0 : 3;
+			/* 0x0 : Ingress Pre-Security MAC Control FIlter
+			 *       (IGPRCTLF) LUT
+			 * 0x1 : Ingress Pre-Security Classification LUT (IGPRC)
+			 * 0x2 : Ingress Packet Format (IGPFMT) SAKey LUT
+			 * 0x3 : Ingress Packet Format (IGPFMT) SC/SA LUT
+			 * 0x4 : Ingress Post-Security Classification LUT
+			 *       (IGPOC)
+			 * 0x5 : Ingress Post-Security MAC Control Filter
+			 *       (IGPOCTLF) LUT
+			 * 0x6 : Ingress MIB (IGMIB)
+			 */
+			unsigned int mssIngressLutSelect : 4;
+		} bits_0;
+		unsigned short word_0;
+	};
 };
 
-
-//---------------------------------------------------------------------------------
-//                  MSS Ingress LUT Control Register: 1E.8081 
-//---------------------------------------------------------------------------------
-struct mssIngressLutControlRegister_t
-{
-  union
-  {
-    struct
-    {
-      unsigned int   reserved0 : 14;
-                    /*! \brief 1E.8081.E R/W MSS Ingress LUT Read
-                        mssIngressLutControlRegister_t.bits_0.mssIngressLutRead
-                        Default = 0x0
-                        1 = LUT read
-                 <B>Notes:</B>
-                        Setting this bit to 1, will read the LUT. This bit will automatically clear to 0.  */
-      unsigned int   mssIngressLutRead : 1;    // 1E.8081.E  R/W      Default = 0x0 
-                     /* 1 = LUT read
-                          */
-                    /*! \brief 1E.8081.F R/W MSS Ingress LUT Write
-                        mssIngressLutControlRegister_t.bits_0.mssIngressLutWrite
-                        Default = 0x0
-                        1 = LUT write
-                 <B>Notes:</B>
-                        Setting this bit to 1, will write the LUT. This bit will automatically clear to 0.  */
-      unsigned int   mssIngressLutWrite : 1;    // 1E.8081.F  R/W      Default = 0x0 
-                     /* 1 = LUT write
-                          */
-    } bits_0;
-    unsigned short word_0;
-  };
+struct mssIngressLutControlRegister_t {
+	union {
+		struct {
+			unsigned int reserved0 : 14;
+			unsigned int mssIngressLutRead : 1;
+			unsigned int mssIngressLutWrite : 1;
+		} bits_0;
+		unsigned short word_0;
+	};
 };
 
 #endif /* MSS_INGRESS_REGS_HEADER */
