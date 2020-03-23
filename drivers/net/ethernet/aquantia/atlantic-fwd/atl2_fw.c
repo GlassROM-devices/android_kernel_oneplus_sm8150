4d8af91c7a03bcc7d64ec497841839f8e2c3e0b3
Dmitry Bogdanov <dbogdanov@marvell.com>
2020-03-23 18:04:44 +0300

    update copyrights and add SPDX tag

--- drivers/net/ethernet/aquantia/atlantic-fwd/atl2_fw.c
+++ drivers/net/ethernet/aquantia/atlantic-fwd/atl2_fw.c
@@ -1,11 +1,13 @@
-/*
- * Marvell Semiconductor Altantic Network Driver
- * Copyright (C) 2019 Marvell Semiconductor. All rights reserved
+// SPDX-License-Identifier: GPL-2.0-only
+/* Atlantic Network Driver
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
+ * Copyright (C) 2019-2020 Marvell International Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  */
+
 #include <linux/etherdevice.h>
 
 #include "atl_common.h"
