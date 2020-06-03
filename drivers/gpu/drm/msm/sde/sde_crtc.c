15f5ecc9b2edb9e16caa52f81c2844d8966ffac9
anupritaisno1 <www.anuprita804@gmail.com>
2020-06-03 06:01:29 +0200

    sde_crtc: revert to oos basically what lineage did but a lot less messed up
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -3414,7 +3414,6 @@ int bl_to_alpha_dc(int brightness)
 	return alpha;
 }
 
-extern int op_dimlayer_bl_enable;
 int oneplus_get_panel_brightness_to_alpha(void)
 {
 	struct dsi_display *display = get_main_display();
@@ -3423,7 +3422,7 @@ int oneplus_get_panel_brightness_to_alpha(void)
 		return 0;
 	if (oneplus_panel_alpha)
 		return oneplus_panel_alpha;
-    if (!op_dimlayer_bl_enable || display->panel->dim_status)
+    if (display->panel->dim_status)
 		return brightness_to_alpha(display->panel->hbm_backlight);
     else
 	return bl_to_alpha_dc(display->panel->hbm_backlight);
@@ -5726,6 +5725,7 @@ int op_dimlayer_bl_enable_real = 0;
 int op_dimlayer_bl = 0;
 bool finger_type = false;
 //extern int aod_layer_hide;
+extern int op_dimlayer_bl_enable;
 extern int op_dp_enable;
 extern int sde_plane_check_fingerprint_layer(const struct drm_plane_state *drm_state);
 static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
@@ -5780,8 +5780,10 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
         if (mode ==3)
             aod_index = i;
 	}
-
-	display->panel->dim_status = fp_index >= 0 && dim_mode != 0;
+	if(fp_index >=0 && dim_mode!=0)
+		display->panel->dim_status = true;
+	else
+		display->panel->dim_status = false;
 
 	if(aod_index <0){
 		oneplus_aod_hid = 0;
@@ -5923,16 +5925,6 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
     }
 	if (fp_index < 0 && !dim_backlight) {
 		cstate->fingerprint_dim_layer = NULL;
-	}
-
-	if (fp_mode == 1) {
-		display->panel->dim_status = true;
-		cstate->fingerprint_pressed = true;
-		return 0;
-	} else if (fp_mode == 0) {
-		display->panel->dim_status = false;
-		cstate->fingerprint_pressed = false;
-		return 0;
 	}
 
 	return 0;
